bceaffcca9954356a85e64d2a6285f513d095f25
panni <panni.work12@gmail.com>
2026-02-19 23:34:27 +0530

    Fi : Test and ci flow
    Key : BOA-15

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,11 +1,11 @@
 from django.http import JsonResponse
 from django.conf import settings
 
+
 # Create your views here.
 def health_check(request):
     return JsonResponse(
         {
-             'version': settings.SPECTACULAR_SETTINGS['VERSION']
+            'version': settings.SPECTACULAR_SETTINGS['VERSION']
             , 'message': 'ok'
-        }
-    )
+        })
